ff0646a2461bd750cf40d820ad1a713201459a74
Adham Khatean <aekhatean@gmail.com>
2022-02-13 20:08:42 +0200

    Add EB variables programatically through Circleci

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,4 +1,5 @@
 eb init $EB_APP_NAME --platform node.js --region $AWS_DEFAULT_REGION
+eb setenv DB_PORT=$DB_PORT JWT_SECRET=$JWT_SECRET PORT=$PORT POSTGRES_DB=$POSTGRES_DB POSTGRES_HOST=$POSTGRES_HOST POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_USERNAME=$POSTGRES_USERNAME
 
 echo "deploy:
   artifact: www/Archive.zip" >> .elasticbeanstalk/config.yml
